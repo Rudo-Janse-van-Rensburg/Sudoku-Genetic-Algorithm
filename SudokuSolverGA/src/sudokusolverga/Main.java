da0fbac01b49f55f0ff344bac0f9e4281f47687e
rudo-synthesis <rudojanse@synthesis.co.za>
2021-05-28 08:52:37 +0200

    change main

--- SudokuSolverGA/src/sudokusolverga/Main.java
+++ SudokuSolverGA/src/sudokusolverga/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,9 +12,7 @@ public class Main {
     private static Chromosome readInSudoku(String puzzleName){
         Integer[][] puzzle = new Integer[boardDim][boardDim];
         try{
-            File file = new File(
-                    "/home/rudo/Desktop/Homework/COS 314/A/02/sudokus/"+puzzleName+".txt"
-            );
+            File file = new File(puzzleName+".txt");
             Scanner sc = null;
             BufferedReader br = new BufferedReader(new FileReader(file));
             int row = 0, col = 0;
@@ -35,9 +34,48 @@ public class Main {
 
         return new Chromosome(puzzle);
     }
+    
+        private static Chromosome readInSudokuString(String puzzleStr){
+        Integer[][] puzzle = new Integer[boardDim][boardDim];
+        try{ 
+            
+            Scanner sc = null;
+            BufferedReader br = new BufferedReader(new StringReader(puzzleStr));
+            int row = 0, col = 0;
+            String line =null;
+            while(row < boardDim && (line = br.readLine()) != null){
+                col = 0;
+                sc = new Scanner(line).useDelimiter(" ");
+                while(sc.hasNext() && col < boardDim ){
+                    puzzle[row][col++] = Integer.parseInt(sc.next().trim());
+                }
+                sc.close();
+                ++row;
+            }
+            br.close();
+        }catch(Exception e){
+                System.err.println("Error: "+e.getMessage());
+        }
+
+
+        return new Chromosome(puzzle);
+    }
+    
 
     public static void main(String[] args) {
-        Chromosome specimen = readInSudoku("s15a");
+        /*Chromosome specimen = readInSudokuString(
+                "0 0 2 0 0 0 5 0 0 \n" +
+                "0 1 0 7 0 5 0 2 0 \n" +
+                "4 0 0 0 9 0 0 0 7 \n" +
+                "0 4 9 0 0 0 7 3 0 \n" +
+                "8 0 1 0 3 0 4 0 9 \n" +
+                "0 3 6 0 0 0 2 1 0 \n" +
+                "2 0 0 0 8 0 0 0 4 \n" +
+                "0 8 0 9 0 2 0 6 0 \n" +
+                "0 0 7 0 0 0 8 0 0 "
+        );*/
+        Chromosome specimen = readInSudoku(args[0]);
+        
         Evolution evolution = evolution = new Evolution(
                 specimen,
                 200000,
